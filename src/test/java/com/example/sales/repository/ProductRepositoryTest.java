c1044091381401ff8615d95d87a06bd6405d43ec
Ronei Macedo Gualberto <ronei@prodam.am.gov.br>
2018-04-09 16:15:00 -0400

    Incluindo novo teste de produto

--- src/test/java/com/example/sales/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/sales/repository/ProductRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.junit.After;
@@ -43,8 +44,31 @@ public class ProductRepositoryTest {
     }
 	
 	@Test
+	public void testSaveWithAllProperties() {
+		
+		final String PRODUCT_NAME = "Product A";
+		final Double PRODUCT_PRICE = 1.20;
+		final String PRODUCT_DESCRIPTION = PRODUCT_NAME + "Description";
+		
+		
+		Product productSaved = ProductBuilder.create()
+			.name(PRODUCT_NAME)
+			.price(PRODUCT_PRICE)
+			.description(PRODUCT_DESCRIPTION)
+			.build();
+		
+		productRepository.save(productSaved);
+		
+		assertEquals(productSaved.getName(),PRODUCT_NAME);
+		assertEquals(productSaved.getPrice(), new BigDecimal(PRODUCT_PRICE));
+		assertEquals(productSaved.getDescription(),PRODUCT_DESCRIPTION);
+		assertNotNull(productSaved.getId());
+    }
+	
+	@Test
 	public void testFindByIdSuccess() {
 		Optional<Product> productFindOpt = this.productRepository.findById(product.getId());
+		
 		Product productFind = productFindOpt.get();
 		
 		assertTrue(productFindOpt.isPresent());
